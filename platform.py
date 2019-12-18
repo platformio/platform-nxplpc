980b2691382d4cac1b519542edd30c0f461b4158
valeros <valeros@bigmir.net>
2019-12-19 01:08:02 +0200

    Update support for Zephyr framework v2.1

--- platform.py
+++ platform.py
@@ -32,8 +32,10 @@ class NxplpcPlatform(PlatformBase):
                 self.packages['tool-pyocd']['type'] = "uploader"
                 
         if "zephyr" in variables.get("pioframework", []):
-            for p in ("framework-zephyr-hal-nxp", "tool-cmake", "tool-dtc", "tool-ninja"):
-                self.packages[p]["optional"] = False
+            for p in self.packages:
+                if p.startswith("framework-zephyr-") or p in (
+                    "tool-cmake", "tool-dtc", "tool-ninja"):
+                    self.packages[p]["optional"] = False
             self.packages['toolchain-gccarmnoneeabi']['version'] = "~1.80201.0"
             if "windows" not in get_systype():
                 self.packages['tool-gperf']['optional'] = False
