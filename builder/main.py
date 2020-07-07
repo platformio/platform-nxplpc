03ea24ae3b461e882efdd129ad0f2e97f4f0110e
Valerii Koval <valeros08@gmail.com>
2020-07-07 14:54:22 +0300

    Add support for new AddPlatformTarget API
    Preparation for upcoming PIO Core 4.4

--- builder/main.py
+++ builder/main.py
@@ -21,6 +21,7 @@ from SCons.Script import (ARGUMENTS, COMMAND_LINE_TARGETS, AlwaysBuild,
                           Builder, Default, DefaultEnvironment)
 
 env = DefaultEnvironment()
+env.SConscript("compat.py", exports="env")
 platform = env.PioPlatform()
 board = env.BoardConfig()
 
