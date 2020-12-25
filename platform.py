336b965e875dae8a25500d6b97f4459c138703c9
valeros <valeros08@gmail.com>
2020-12-25 18:29:59 +0200

    Update mbed packages

--- platform.py
+++ platform.py
@@ -39,7 +39,7 @@ class NxplpcPlatform(PlatformBase):
                 if os.path.isfile(deprecated_boards_file):
                     with open(deprecated_boards_file) as fp:
                         if board in json.load(fp):
-                            self.packages["framework-mbed"]["version"] = "~6.51504.0"
+                            self.packages["framework-mbed"]["version"] = "^6.51504.0"
                 self.packages["toolchain-gccarmnoneeabi"]["version"] = "~1.90201.0"
 
         if "zephyr" in variables.get("pioframework", []):
